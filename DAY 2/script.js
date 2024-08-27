fb658874a3e4daa44a3abce0758fd757c2dbc0d0
Balasundar03 <balasundar.m3@gmail.com>
2024-08-27 23:23:05 +0530

    first commit

---
+++ DAY 2/script.js
@@ -0,0 +1,67 @@
+// practice java script
+// 1 : Alert & prompts
+ 
+// alert("Email is not found") //its has only OK Button.
+
+
+//prompts
+
+//  let names=prompt("enter the value") //it's shows OK & CANCEL button.
+// console.log(names);
+
+
+// additional examble prompts
+
+// let a=prompt("enter the value") 
+// console.log(a+a+a); //it's shows sting value 3 times
+
+
+// 2: Operators - Arithmetic & Logical
+// let s=10;
+// let v=5;
+
+// console.log(s+v);
+// console.log(s-v);
+// console.log(s*v);
+// console.log(s/v);
+// console.log(s%v);
+// console.log(s*v);
+
+
+// Logical Operators
+
+// let s= true
+// let v= false
+// console.log(s && v); //false
+// console.log(s || v); //ture
+// console.log(!s); //false
+// console.log(!v); //true
+
+// let s=10;
+// let v=20;
+
+// console.log(s==10 && v==20); //true
+// console.log(s==10 && v==50); //falsae
+// console.log(s==10 || v==20); //true
+// console.log(!s==10); //false
+
+// Variables & Data Types 
+
+// var name="sundar";
+// console.log(name); // var define Global Scope
+
+// var age=18
+// console.log(age); // let define Local Scope
+
+// const country="India";
+// console.log(country); // const define cannot reassigned
+
+
+
+
+
+
+
+
+
+
